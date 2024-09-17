421b11f533dddbdb499beb709abb66c01f27665b
Justice Philemon <imailasjay>
2024-09-17 12:12:22 +0100

    data types.

---
+++ 2_data_types.py
@@ -0,0 +1,14 @@
+# Python Data Types
+
+# you can get the data type of any object with the type() function.
+print(type(123)) # prints: <class 'int'>
+
+# settig a data type for a variable
+a = str("hello")
+b = int(123)
+c = bool(5)
+d = float(23.1)
+e = list((1, "hey", True))
+f = tuple((1, "hey", False))
+g = dict(name = "Jon", age = 12)
+h = set(("apple", 32, []))
